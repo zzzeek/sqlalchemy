2997668bc47a963d4890e927c13d5c06a996df40
Mike Bayer <mike_mp@zzzcomputing.com>
2014-07-20 15:37:02 -0400

    more changes

--- lib/sqlalchemy/engine/base.py
+++ lib/sqlalchemy/engine/base.py
@@ -86,7 +86,7 @@ class Connection(Connectable):
 
         print("hi!"
                 "Im not flake8!"
-                "wrongo!"
+                "more changes"
             )
     def _branch(self):
         """Return a new Connection which references this Connection's
