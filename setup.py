584fca1654b780b137f47efb8532bc0f69de9acc
Hugo <hugovk@users.noreply.github.com>
2018-08-26 19:53:12 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -141,6 +141,7 @@ def run_setup(with_cext):
         cmdclass=cmdclass,
         tests_require=['pytest >= 2.5.2', 'mock', 'pytest-xdist'],
         long_description=readme,
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
